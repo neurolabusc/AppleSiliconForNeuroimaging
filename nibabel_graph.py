b506c887767f23877ddb650f111c5a4db336b749
neurolabusc <crorden6@gmail.com>
2022-06-23 08:03:41 -0400

    Python 1.23, M2

--- nibabel_graph.py
+++ nibabel_graph.py
@@ -11,8 +11,8 @@ import matplotlib.pyplot as plt
 #MacBook i5-8259U
 #Ubuntu i7-8700K
 #Ubuntu 3900X
-df = pd.DataFrame({'CPU': ['8259U','3900X','M1-x86', 'M1-ARM'],
-					'Time': [26.5, 12.418, 14.08, 12.934]})
+df = pd.DataFrame({'CPU': ['8259U','3900X','M1-x86', 'M1-ARM', 'M1-ARM-1.23',],
+					'Time': [26.5, 12.418, 14.08, 12.934, 10.563]})
 sns.set()
 ax = sns.catplot(x="CPU", y="Time", kind="bar", data=df)
 plt.savefig('nibabel.png', dpi=300)
